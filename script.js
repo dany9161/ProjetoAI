401d1b516ffdd09dc8307d5d3fa8099d18bd0200
dany9161 <16843934+dany9161@users.noreply.github.com>
2022-01-07 10:00:57 +0000

    v de à uma semana

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const MinWidth = 650;//MinWidth to change responsiveness
+const MinWidth = 590;//MinWidth to change responsiveness
 
 function setUp() {
     //console.log(window.innerWidth);
@@ -28,6 +28,7 @@ function navChangeClass(todo){
     );
 }
 
+//quando a pagina passa a ter largura para mostrar o menu, esta função esconde o menu
 function escondeMenu(){
     var items = document.getElementsByClassName("navItem");
     Array.from(items).forEach(function(item){
@@ -35,6 +36,7 @@ function escondeMenu(){
     });
 }
 
+//Mostra / esconde o menu (quando está responsivo)
 function toggleMenu(container){
     //console.log("toggleMenu");
     var items = document.getElementsByClassName("navItem");
@@ -43,4 +45,23 @@ function toggleMenu(container){
         item.classList.toggle("block");
     });
     container.classList.toggle("change");
+}
+
+function showTratS(){
+    var tabS = document.getElementsByClassName("tratS")[1];
+    var tabH = document.getElementsByClassName("tratH")[1];
+    tabS.style.display = "block";
+    tabH.style.display = "none";
+
+    document.getElementsByClassName("tabS")[0].classList.add("active");
+    document.getElementsByClassName("tabH")[0].classList.remove("active");
+}
+
+function showTratH(){
+    var tabS = document.getElementsByClassName("tratS")[1];
+    var tabH = document.getElementsByClassName("tratH")[1];
+    tabH.style.display = "block";
+    tabS.style.display = "none";
+    document.getElementsByClassName("tabS")[0].classList.remove("active");
+    document.getElementsByClassName("tabH")[0].classList.add("active");
 }
